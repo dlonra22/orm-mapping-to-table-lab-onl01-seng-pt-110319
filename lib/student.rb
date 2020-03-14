08df1b4ebfb527a19f58c28ef9880e27f646011b
arnie b <arnold.bwaila@gmail.com>
2020-03-14 17:53:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,9 +2,7 @@ class Student
 attr_accessor :name, :grade
 attr_reader :id
 
- @@all = []
- 
- def initialize(name, grade, id=nil)
+  def initialize(name, grade, id=nil)
    @id = nil
    @name = name
    @grade = grade
